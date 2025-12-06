a1db3ccb826afb9d0ee48048a411429eb7c80735
Parwez Aalam <parwezak5050@gmail.com>
2025-12-06 18:51:44 +0530

    config added

---
+++ lib/Agora/agora_config.dart
@@ -0,0 +1,7 @@
+class AgoraConfig {
+  // Get your App ID from https://console.agora.io/
+  static const String appId = '06ae030cf59f4cf2a7704bc56ef98d36';
+
+  // You can use any channel name
+  static const String token = ''; // Optional: use null for testing
+}
